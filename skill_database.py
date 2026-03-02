a4caf308441a4f61708cd8da49e9680aa0f4da55
kavitharaja02 <kavitharaja4100@gmail.com>
2026-03-02 23:46:21 +0530

    Add initial skills list to skill_database.py

---
+++ skill_database.py
@@ -0,0 +1,8 @@
+skills = [
+    "python", "java", "c", "c++", "sql",
+    "machine learning", "deep learning",
+    "data analysis", "html", "css",
+    "javascript", "react", "node",
+    "arduino", "iot", "automation",
+    "git", "github", "linux"
+]
